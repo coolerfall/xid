a4d50cf9b5db6ea12e6dff697c25b067e4cc7cce
Vincent Cheung <coolingfall@gmail.com>
2019-11-13 15:42:02 +0800

    Bumped version number to 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.coolerfall"
-version = "1.0.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
   mavenCentral()
@@ -28,8 +28,10 @@ publishing {
         description.set("A port of xid in Java")
         url.set("https://github.com/coolerfall/xid")
         licenses {
-          name.set("The Apache License, Version 2.0")
-          url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+          license {
+            name.set("The Apache License, Version 2.0")
+            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+          }
         }
         developers {
           developer {
